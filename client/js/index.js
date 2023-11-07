6d32c1cabfc9088b1e16ac75988b467cfa9b3653
SofiaaSottile18 <112595421+SofiaaSottile18@users.noreply.github.com>
2023-11-07 20:20:30 -0300

    Cargando archivos para funcionalidades

---
+++ client/js/index.js
@@ -0,0 +1,39 @@
+const shopContent = document.getElementById("shopContent");
+const cart = [];
+
+productos.forEach((product) => {
+    const content = document.createElement("div");
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p class="price">$ ${product.price}</p>
+    `;
+    shopContent.append(content);
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+
+    content.append(buyButton);
+
+    buyButton.addEventListener("click", () => {
+        const repeat = cart.some((prodRepe) => prodRepe.id === product.id);
+
+        if (repeat) {
+            cart.map((p) => {
+                if (p.id === product.id) {
+                    p.quantity++;
+                    displayCartCounter();
+                }
+            })
+        } else {
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quantity: product.quantity,
+                img: product.img,
+            })
+            displayCartCounter();
+        }
+    })
+});
---
+++ client/js/products.js
@@ -0,0 +1,142 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Vino 1",
+        price: 480,
+        quantity: 1,
+        img: "./media/vino-1.jpg",
+    },
+    {
+        id: 2,
+        productName: "Vino 2",
+        price: 1200,
+        quantity: 1,
+        img: "./media/vino-2.jpg",
+    },
+    {
+        id: 3,
+        productName: "Vino 3",
+        price: 1100,
+        quantity: 1,
+        img: "./media/vino-3.jpg",
+    },
+    {
+        id: 4,
+        productName: "Vino 4",
+        price: 800,
+        quantity: 1,
+        img: "./media/vino-4.jpg",
+    },
+    {
+        id: 5,
+        productName: "Vino 5",
+        price: 380,
+        quantity: 1,
+        img: "./media/vino-5.jpg",
+    },
+    {
+        id: 6,
+        productName: "Vino 6",
+        price: 690,
+        quantity: 1,
+        img: "./media/vino-1.jpg",
+    },
+    {
+        id: 7,
+        productName: "Vino 7",
+        price: 540,
+        quantity: 1,
+        img: "./media/vino-2.jpg",
+    },
+    {
+        id: 8,
+        productName: "Vino 8",
+        price: 1500,
+        quantity: 1,
+        img: "./media/vino-3.jpg"
+    },
+    {
+        id: 9,
+        productName: "Vino 9",
+        price: 1000,
+        quantity: 1,
+        img: "./media/vino-4.jpg"
+    },
+    {
+        id: 10,
+        productName: "Vino 10",
+        price: 1590,
+        quantity: 1,
+        img: "./media/vino-5.jpg"
+    },
+    {
+        id: 11,
+        productName: "Vino 11",
+        price: 1770,
+        quantity: 1,
+        img: "./media/vino-1.jpg"
+    },
+    {
+        id: 12,
+        productName: "Vino 12",
+        price: 1100,
+        quantity: 1,
+        img: "./media/vino-2.jpg"
+    },
+    {
+        id: 13,
+        productName: "Vino 13",
+        price: 1200,
+        quantity: 1,
+        img: "./media/vino-3.jpg"
+    },
+    {
+        id: 14,
+        productName: "Vino 14",
+        price: 1980,
+        quantity: 1,
+        img: "./media/vino-4.jpg"
+    },
+    {
+        id: 15,
+        productName: "Vino 15",
+        price: 1580,
+        quantity: 1,
+        img: "./media/vino-5.jpg"
+    },
+    {
+        id: 16,
+        productName: "Vino 16",
+        price: 1540,
+        quantity: 1,
+        img: "./media/vino-1.jpg"
+    },
+    {
+        id: 17,
+        productName: "Vino 17",
+        price: 1380,
+        quantity: 1,
+        img: "./media/vino-2.jpg"
+    },
+    {
+        id: 18,
+        productName: "Vino 18",
+        price: 1900,
+        quantity: 1,
+        img: "./media/vino-3.jpg"
+    },
+    {
+        id: 19,
+        productName: "Vino 19",
+        price: 1800,
+        quantity: 1,
+        img: "./media/vino-4.jpg"
+    },
+    {
+        id: 20,
+        productName: "Vino 20",
+        price: 2000,
+        quantity: 1,
+        img: "./media/vino-5.jpg"
+    },
+]
